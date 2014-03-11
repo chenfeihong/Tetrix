dcc91960ef3b3db00bb8fe80c2b5a6d6540a89a9
liujiaxing <liujiaxingemail@gmail.com>
2014-03-11 16:38:14 +0800

    change

--- tetrix.js
+++ tetrix.js
@@ -65,12 +65,12 @@ for(var i = 0; i < coords.length; i++){
 }
 
 function Shape(){ //形状对象	
-	this.x = 0,
-	this.y = 0,
-	this.shape = null,
-	this.color = null,
-	this.index = null,
-	this.direction = null,
+	this.x = 0;
+	this.y = 0;
+	this.shape = null;
+	this.color = null;
+	this.index = null;
+	this.direction = null;
 	this.maxX = function(){  //形状的最大宽度,用于边界判断 --7
 		if(!this.shape){return null;};
 		var count = 0;
@@ -83,7 +83,7 @@ function Shape(){ //形状对象
 			}
 		}
 		return count;
-	},
+	};
 	this.maxY = function(){  //形状的最大高度,,用于边界判断 --6
 		if(!this.shape){return null;};
 		var count = 0;
@@ -96,7 +96,7 @@ function Shape(){ //形状对象
 			}
 		}
 		return count;
-	},
+	};
 	this.marginLeft = function(){	//模型左边的空列数
 		if(!this.shape){return null;};
 		var count = 0;
@@ -114,7 +114,7 @@ function Shape(){ //形状对象
 			}
 		}
 		return count;
-	},
+	};
 	this.marginTop = function(){	//模型顶部的空行数
 		if(!this.shape){return null;};
 		var count = 0;
@@ -132,7 +132,10 @@ function Shape(){ //形状对象
 			}
 		}
 		return count;
-	}
+	};
+	this.toString = function(){
+		console.log("[x : "+this.x+" y:"+this.y+" maxX:"+this.maxX()+" maxY:"+this.maxY()+" marginLeft:"+this.marginLeft()+" marginTop:"+this.marginTop()+"]");
+	};
 }
 
 var currentShape = null;
@@ -170,7 +173,6 @@ function drawSquare(selectedShape){
 	for(var x = 0; x < 4; x++){
 		for(var y = 0; y < 4; y++ ){
 			if(selectedShape.shape[x][y] != 0){
-				//console.log("left:"+selectedShape.marginLeft() + "; top:"+selectedShape.marginTop());
 				//一个方块的位置是由Board左上角(0,0),然后是selectedShape的左上角位置决定
 				drawCell(x - selectedShape.marginLeft() + selectedShape.x,y - selectedShape.marginTop() + selectedShape.y ,selectedShape.color);
 			}
@@ -179,74 +181,53 @@ function drawSquare(selectedShape){
 }
 
 //移动方法 --4
-function tryMove(){
+function tryMove(shape,newX,newY){
 	//越界判断
-	currentShape.x = currentShape.x < 0 ? currentShape.x += 1 : currentShape.x;
-	currentShape.y = currentShape.y < 0 ? currentShape.y += 1 : currentShape.y;
+	if(newX < 0){return false;};
 	//右边、下边越界
-	console.log("y: "+ currentShape.y);
-	console.log("width: "+ currentShape.maxX() + "; height: " + currentShape.maxY());
-	if(currentShape.x + currentShape.maxX() > column){
-		currentShape.x -= 1;
+	if(newX + shape.maxX() > column){
 		return false;
-	}
-	if(currentShape.y + currentShape.maxY() > row){
-		currentShape.y -= 1;
+	}else if(newY + shape.maxY() > row){
 		return false;
-	}
-	for(var i = 0; i < 4; i++){
-		for(var j = 0; j < 4; j++){
-			if(shapeAt(currentShape.x - currentShape.marginLeft() + i,currentShape.y - currentShape.marginTop() + j)){
-				return false;
+	}else{
+		for(var i = 0; i < 4; i++){
+			for(var j = 0; j < 4; j++){
+				if(shape.shape[i][j] != 0 && coords[newY + j - shape.marginTop()][newX + i - shape.marginLeft()]){
+					return false;
+				}
 			}
 		}
 	}
+	shape.x = newX;
+	shape.y = newY;
+	currentShape = shape;
 	return true;
-}
-
-//产生随机0-seed但是不包含seed的整数。
-function randomNumber(seed){
-	if(!seed) return -1;
-	return Math.floor(Math.random() * seed);
-}
-
-//查询该处是否已经存在方块
-function shapeAt(x,y){
-	//console.log(x+" "+y);
-	var color = coords[x][y];
-	if(color){
-		return true;
-	}else{
-		return false;
-	}
 }
 
 //全局键盘事件监听 -- 5
 document.onkeydown = function(e){
 	if(currentShape == null){return;};
 	if(e.keyCode == 37){		//left
-		currentShape.x -= 1;
+		tryMove(currentShape,currentShape.x - 1,currentShape.y);
 	}else if(e.keyCode == 38){	//up
 		//旋转向右
 		currentShape.direction = currentShape.direction - 1 < 0 ? 3 : currentShape.direction - 1;
 		currentShape.shape = shapes[currentShape.index][currentShape.direction];
 		currentShape.color = colors[currentShape.index];
 	}else if(e.keyCode == 39){	//right
-		currentShape.x += 1;
+		tryMove(currentShape,currentShape.x + 1,currentShape.y);
 	}else if(e.keyCode == 40){	//down
-		currentShape.y += 1;
-	}
-	if(!tryMove()){	//无法移动则将当前Shape赋值给全局的二维数组
-		for(var i = 0; i < 4; i++){
-			for(var j = 0; j < 4; j++){
-				if(currentShape.shape[i][j] != 0){
-					//console.log((currentShape.x + i) + " " + (currentShape.y + j));
-					coords[currentShape.y + j][currentShape.x + i] = currentShape.color;
+		if(!tryMove(currentShape,currentShape.x,currentShape.y + 1)){
+			for(var i = 0; i < 4; i++){
+				for(var j = 0; j < 4; j++){
+					if(currentShape.shape[i][j] != 0){
+						coords[currentShape.y + j - currentShape.marginTop()][currentShape.x + i - currentShape.marginLeft()] = currentShape.color;
+					}
 				}
 			}
+			currentShape = nextShape;
+			nextShape = createShape();	
 		}
-		currentShape = nextShape;
-		nextShape = createShape();	
 	}
 	//刷新重绘
 	update();
@@ -272,10 +253,11 @@ function update(){
 	drawSquare(currentShape);
 }
 
+//创建
 function createShape(){
 	var newShape = new Shape();
-	newShape.index = randomNumber(7);
-	newShape.direction = randomNumber(4);
+	newShape.index = Math.floor(Math.random() * 7);
+	newShape.direction = Math.floor(Math.random() * 4);
 	newShape.shape = shapes[newShape.index][newShape.direction];
 	newShape.color = colors[newShape.index];
 	return newShape;
